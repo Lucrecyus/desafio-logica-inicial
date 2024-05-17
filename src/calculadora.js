9cbad6bd6818d8cd1942e0730a04b226f490a5e8
lucrecyus <lucrecyus@gmail.com>
2024-05-17 17:01:08 -0300

    desafio calculadora hankeada

---
+++ src/calculadora.js
@@ -0,0 +1,30 @@
+
+let jogadorVitorias = 102
+let jogadorDerrotas = 20
+let resultado = quantidadeVitorias(jogadorVitorias, jogadorDerrotas);
+
+
+function quantidadeVitorias(vitorias,derrotas) {
+
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if(vitorias < 10){
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+    return {saldoVitorias, nivel};
+}
+
+console.log("O Herói tem de saldo de " + resultado.saldoVitorias, "está no nível de " + resultado.nivel)
